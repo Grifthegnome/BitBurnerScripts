9390aa147dabb8d2395674b570bd530085328b8e
Grifthegnome <grifbd@aol.com>
2025-02-13 18:36:58 -0800

    Modified net-backtrace.js to include backdoor helpers.

--- net-backtrace.js
+++ net-backtrace.js
@@ -1,61 +1,67 @@
 /** @param {NS} ns */
 export async function main(ns) {
 
-  //run4theh111z
-
-  const serverToFind = ns.args[0]
-
-  const backtrackStack = FindServerAndBackTrace( ns, "home", "home", serverToFind )
-
-  if ( backtrackStack.length > 0 )
-  {
-    for ( let i = 0; i < backtrackStack.length; i++ )
+    //run4theh111z
+  
+    const serverToFind = ns.args[0]
+  
+    const backtrackStack = FindServerAndBackTrace( ns, "home", "home", serverToFind )
+  
+    if ( backtrackStack.length > 0 )
     {
-      const backtrackServer = backtrackStack[i]
-      ns.tprint( backtrackServer )
+      for ( let i = 0; i < backtrackStack.length; i++ )
+      {
+        const backtrackServer = backtrackStack[i]
+  
+        const serverInfo = ns.getServer( backtrackServer )
+  
+        const printString = serverInfo.backdoorInstalled ? backtrackServer + " ===> HAS BACKDOOR " : backtrackServer  
+  
+        ns.tprint( printString )
+      }
     }
-  }
-  else
-  {
-    ns.tprint( "Could Not Back Track Server " + serverToFind + ", server not found in network." )
-  }
-
-}
-
-function FindServerAndBackTrace( ns, hostServer, parentServer, serverToFind )
-{
-  //This should be called with "home" as the starting server by the caller.
-  const connections = ns.scan( hostServer )
-
-  let backtrackStack = Array()
-
-  for ( let i = 0; i < connections.length; i++ )
-  {
-    const currentConnection = connections[ i ]
-
-    if ( currentConnection == parentServer )
-      continue
-
-    if ( currentConnection == serverToFind )
+    else
     {
-      ns.tprint( "Server " + serverToFind + " found, backtracing to home" )
-      backtrackStack.push( currentConnection )
-
-      return backtrackStack
+      ns.tprint( "Could Not Back Track Server " + serverToFind + ", server not found in network." )
     }
+  
+  }
+  
+  function FindServerAndBackTrace( ns, hostServer, parentServer, serverToFind )
+  {
+    //This should be called with "home" as the starting server by the caller.
+    const connections = ns.scan( hostServer )
+  
+    let backtrackStack = Array()
+  
+    for ( let i = 0; i < connections.length; i++ )
+    {
+      const currentConnection = connections[ i ]
+  
+      if ( currentConnection == parentServer )
+        continue
+  
+      if ( currentConnection == serverToFind )
+      {
+        ns.tprint( "Server " + serverToFind + " found, backtracing to home" )
+        backtrackStack.push( currentConnection )
+  
+        return backtrackStack
+      }
+      
+      //Search processes in sub-networks.
+      const searchResults = FindServerAndBackTrace( ns, currentConnection, hostServer, serverToFind )
     
-    //Search processes in sub-networks.
-    const searchResults = FindServerAndBackTrace( ns, currentConnection, hostServer, serverToFind )
+      if ( searchResults.length > 0 )
+      {
+        backtrackStack.push( currentConnection )
+        backtrackStack = backtrackStack.concat( searchResults )
   
-    if ( searchResults.length > 0 )
-    {
-      backtrackStack.push( currentConnection )
-      backtrackStack = backtrackStack.concat( searchResults )
-
-      return backtrackStack
+        return backtrackStack
+      }
+    
     }
   
+    return backtrackStack
   }
-
-  return backtrackStack
-}
+  
