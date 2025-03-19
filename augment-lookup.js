1111c99a111471de73963a9debaa78403b97ae35
Grifthegnome <grifbd@aol.com>
2025-03-18 18:26:08 -0700

    augment-lookup - changed formatting to match game's stat formatting.

--- augment-lookup.js
+++ augment-lookup.js
@@ -9,7 +9,27 @@ export async function main(ns)
     return
   }
 
-  const augmentationName = ns.args[0]
+  let augmentationName = ""
+  if ( ns.args.length > 1 )
+  {
+    for ( let i = 0; i < ns.args.length; i++ )
+    {
+      const augmentNameSegment = ns.args[i]
+      if ( i != ns.args.length - 1 )
+      {
+        augmentationName += (augmentNameSegment + " ")
+      }
+      else
+      {
+        augmentationName += augmentNameSegment
+      }
+    }
+  }
+  else
+  {
+    augmentationName = ns.args[0]
+  }
+  
 
   const augmentStats = ns.singularity.getAugmentationStats( augmentationName )
 
@@ -19,100 +39,100 @@ export async function main(ns)
 
   //Stats
   if ( augmentStats.hacking > 1 )
-    ns.tprint( "Hacking: " + ConvertPositiveValueToPercent(  augmentStats.hacking ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.hacking ) + " hacking skill"  )
 
   if ( augmentStats.hacking_exp > 1 )
-    ns.tprint( "Hacking Exp: " + ConvertPositiveValueToPercent(  augmentStats.hacking_exp ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.hacking_exp ) + " hacking exp" )
 
   if ( augmentStats.hacking_chance > 1 )
-    ns.tprint( "Hacking Chance: " + ConvertPositiveValueToPercent(  augmentStats.hacking_chance ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.hacking_chance ) + " hack() success chance"  )
 
   if ( augmentStats.hacking_grow > 1 )
-    ns.tprint( "Hacking Grow: " + ConvertPositiveValueToPercent(  augmentStats.hacking_grow ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.hacking_grow ) + " hacking grow"  )
 
   if ( augmentStats.hacking_money > 1 )
-    ns.tprint( "Hacking Money: " + ConvertPositiveValueToPercent(  augmentStats.hacking_money ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.hacking_money ) + " hack() power" )
 
   if ( augmentStats.hacking_speed > 1 )
-    ns.tprint( "Hacking Speed: " + ConvertPositiveValueToPercent(  augmentStats.hacking_speed ) )
+    ns.tprint( ConvertPositiveValueToPercent( augmentStats.hacking_speed ) + " faster hack(), grow(), and weaken()" )
 
   if ( augmentStats.strength > 1 )
-    ns.tprint( "Strength: " + ConvertPositiveValueToPercent(  augmentStats.strength ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.strength ) + " strength skill" )
 
   if ( augmentStats.strength_exp > 1 )
-    ns.tprint( "Strength Exp: " + ConvertPositiveValueToPercent(  augmentStats.strength_exp ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.strength_exp ) + " strength exp" )
 
   if ( augmentStats.defense > 1 )
-    ns.tprint( "Defense: " + ConvertPositiveValueToPercent(  augmentStats.defense ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.defense ) + " defense skill"  )
 
   if ( augmentStats.defense_exp > 1 )
-    ns.tprint( "Defense Exp: " + ConvertPositiveValueToPercent(  augmentStats.defense_exp ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.defense_exp ) + " defense exp" )
 
   if ( augmentStats.dexterity > 1 )
-    ns.tprint( "Dexterity: " + ConvertPositiveValueToPercent(  augmentStats.dexterity ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.dexterity ) + " dexterity skill"  )
 
   if ( augmentStats.dexterity_exp > 1 )
-    ns.tprint( "Dexterity Exp: " + ConvertPositiveValueToPercent(  augmentStats.dexterity_exp ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.dexterity_exp ) + " dexterity exp" )
 
   if ( augmentStats.agility > 1 )
-    ns.tprint( "Agility: " + ConvertPositiveValueToPercent( augmentStats.agility ) )
+    ns.tprint( ConvertPositiveValueToPercent( augmentStats.agility ) + " agility skill"  )
 
   if ( augmentStats.agility_exp > 1 )
-    ns.tprint( "Agility Exp: " + ConvertPositiveValueToPercent( augmentStats.agility_exp ) )
+    ns.tprint( ConvertPositiveValueToPercent( augmentStats.agility_exp ) + " agility exp" )
   
   if ( augmentStats.charisma > 1 )
-    ns.tprint( "Charisma: " + ConvertPositiveValueToPercent( augmentStats.charisma ) )
+    ns.tprint( ConvertPositiveValueToPercent( augmentStats.charisma ) + " charisma skill"  )
 
   if ( augmentStats.charisma_exp > 1 )
-    ns.tprint( "Charisma Exp: " + ConvertPositiveValueToPercent(  augmentStats.charisma_exp ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.charisma_exp ) + " charisma exp" )
   
 
   //Crime
   if ( augmentStats.crime_money > 1 )
-    ns.tprint( "Crime Money: " + ConvertPositiveValueToPercent(  augmentStats.crime_money ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.crime_money ) + " crime money" )
 
   if ( augmentStats.crime_success > 1 )
-    ns.tprint( "Crime Success: " + ConvertPositiveValueToPercent(  augmentStats.crime_success ) )
+    ns.tprint( ConvertPositiveValueToPercent( augmentStats.crime_success ) + " crime success chance" )
 
   //Faction
   if ( augmentStats.faction_rep > 1 )
-    ns.tprint( "Faction Rep: " + ConvertPositiveValueToPercent(  augmentStats.faction_rep ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.faction_rep ) + " reputation from factions" )
 
   //Company
   if ( augmentStats.company_rep > 1 )
-    ns.tprint( "Company Rep: " + ConvertPositiveValueToPercent(  augmentStats.company_rep ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.company_rep ) + " reputation from companies" )
 
   if ( augmentStats.work_money > 1 )
-    ns.tprint( "Work Money: " + ConvertPositiveValueToPercent(  augmentStats.work_money ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.work_money ) + " work money" )
   
   //Hacknet
   if ( augmentStats.hacknet_node_purchase_cost < 1 )
-    ns.tprint( "Hacknet Purchase Cost: " + -( 1 - augmentStats.hacknet_node_purchase_cost ).toFixed(2) * 100 + "%" )
+    ns.tprint( ConvertNegativeValueToPercent( augmentStats.hacknet_node_purchase_cost ) + " hacknet purchase cost" )
 
   if ( augmentStats.hacknet_node_level_cost < 1 )
-    ns.tprint( "Hacknet Level Cost: " + -( 1 - augmentStats.hacknet_node_level_cost ).toFixed(2) * 100 + "%" )
+    ns.tprint( ConvertNegativeValueToPercent( augmentStats.hacknet_node_level_cost ) + " hacknet level cost" )
 
   if ( augmentStats.hacknet_node_ram_cost < 1 )
-    ns.tprint( "Hacknet RAM Cost: " +  -( 1 - augmentStats.hacknet_node_ram_cost ).toFixed(2) * 100 + "%" )
+    ns.tprint( ConvertNegativeValueToPercent( augmentStats.hacknet_node_ram_cost ) + " hacknet RAM cost: "  )
 
   if ( augmentStats.hacknet_node_core_cost < 1 )
-    ns.tprint( "Hacknet Core Cost: " + -( 1 - augmentStats.hacknet_node_core_cost ).toFixed(2) * 100 + "%" )
+    ns.tprint( ConvertNegativeValueToPercent( augmentStats.hacknet_node_core_cost ) + " hacknet core cost: " )
 
   if ( augmentStats.hacknet_node_money > 1 )
-    ns.tprint( "Hacknet Money: " + Math.round(( augmentStats.hacknet_node_money - 1 ) * 100) + "%" )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.hacknet_node_money ) + " hacknet money" )
 
   //Blade Burner
   if ( augmentStats.bladeburner_analysis > 1 )
-    ns.tprint( "Bladeburner Analysis: " + ConvertPositiveValueToPercent( augmentStats.bladeburner_analysis ) )
+    ns.tprint( ConvertPositiveValueToPercent( augmentStats.bladeburner_analysis ) + " bladeburner analysis" )
 
   if ( augmentStats.bladeburner_max_stamina > 1 )
-    ns.tprint( "Bladeburner Max Stamina: " + ConvertPositiveValueToPercent( augmentStats.bladeburner_max_stamina ) )
+    ns.tprint( ConvertPositiveValueToPercent( augmentStats.bladeburner_max_stamina ) + " bladeburner max stamina" )
 
   if ( augmentStats.bladeburner_stamina_gain > 1 )
-    ns.tprint( "Bladeburner Stamina Gain: " + ConvertPositiveValueToPercent(  augmentStats.bladeburner_stamina_gain ) )
+    ns.tprint( ConvertPositiveValueToPercent(  augmentStats.bladeburner_stamina_gain ) + " bladeburner stamina gain" )
 
   if ( augmentStats.bladeburner_success_chance > 1 )
-    ns.tprint( "Bladeburner Success Chance: " + ConvertPositiveValueToPercent( augmentStats.bladeburner_success_chance ) )
+    ns.tprint( ConvertPositiveValueToPercent( augmentStats.bladeburner_success_chance ) + " bladeburner success chance" )
 
 
   if ( augmentationName == "CashRoot Starter Kit" )
@@ -135,5 +155,10 @@ export async function main(ns)
 
 function ConvertPositiveValueToPercent( value )
 {
-  return Math.round(( value - 1 ) * 100) + "%"
+  return "+" + Math.round(( value - 1 ) * 100) + "%"
+}
+
+function ConvertNegativeValueToPercent( value )
+{
+  return -( 1 - value ).toFixed(2) * 100 + "%"
 }
